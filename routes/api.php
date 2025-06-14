2bf03fdca0a3277dc4857f1f2de2ff36c3656acb
Yukesh Katuwal <93563263+Ukeshgit@users.noreply.github.com>
2025-06-14 13:58:17 +0545

    code revised and maintaind

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
-
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
-Route::middleware('api')->group(function () {
-    Route::post('/auth/register', [UserController::class, 'createUser']);
-    Route::post('/auth/login', [UserController::class, 'loginUser']);
-});
+Route::post('/auth/register', [UserController::class, 'createUser']);
+Route::post('/auth/login', [UserController::class, 'loginUser']);
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,3 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return 'This is the About page.';
 });
-use App\Http\Controllers\Api\UserController;
-
-Route::middleware('api')->group(function () {
-    Route::post('/auth/register', [UserController::class, 'createUser']);
-    Route::post('/auth/login', [UserController::class, 'loginUser']);
-});
